d9ae44569416b5855f2cf41e7d332168e25fc374
Graziano Bolla <grazianobolla@gmail.com>
2025-11-15 15:49:36 -0300

    updated to godot 4.5.1

--- demo/MainScene.cs
+++ demo/MainScene.cs
@@ -6,25 +6,25 @@ namespace GameDemo;
 
 public partial class MainScene : Node3D
 {
-    // When the client clicks "Spawn" we request the server to spawn a Player entity for us
-    private void OnSpawnButtonPressed()
-    {
-        ClientManager.Instance.MakeEntityRequest((byte)GameEntitySpawner.EntityType.Player);
-        GetNode("Menu/SpawnButton").QueueFree();
-    }
+	// When the client clicks "Spawn" we request the server to spawn a Player entity for us
+	private void OnSpawnButtonPressed()
+	{
+		ClientManager.Instance.MakeEntityRequest((byte)GameEntitySpawner.EntityType.Player);
+		GetNode("Menu/SpawnButton").QueueFree();
+	}
 
-    // Creates game server
-    private void OnHostButtonPressed()
-    {
-        MonkeNetManager.Instance.CreateServer(9999);
-        GetNode("Menu").QueueFree();
-    }
+	// Creates game server
+	private void OnHostButtonPressed()
+	{
+		MonkeNetManager.Instance.CreateServer(9999);
+		GetNode("Menu").QueueFree();
+	}
 
-    // Creates Client and connects to 
-    private void OnConnectButtonPressed()
-    {
-        MonkeNetManager.Instance.CreateClient("localhost", 9999);
-        GetNode("Menu/HostButton").QueueFree();
-        GetNode("Menu/ConnectButton").QueueFree();
-    }
+	// Creates Client and connects to 
+	private void OnConnectButtonPressed()
+	{
+		MonkeNetManager.Instance.CreateClient("localhost", 9999);
+		GetNode("Menu/HostButton").QueueFree();
+		GetNode("Menu/ConnectButton").QueueFree();
+	}
 }
