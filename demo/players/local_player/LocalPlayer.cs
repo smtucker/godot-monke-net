da97884d77a9fb9b18674a8fd0505a7e0c7d6671
Graziano Bolla <grazianobolla@gmail.com>
2025-11-15 17:42:22 -0300

    log position

--- demo/players/local_player/LocalPlayer.cs
+++ demo/players/local_player/LocalPlayer.cs
@@ -49,6 +49,7 @@ public partial class LocalPlayer : CharacterBody3D, IPredictableEntity
         if (ImGui.Begin("Player Information"))
         {
             ImGui.Text("Use `C` to free/capture your cursor.");
+            ImGui.Text($"Position ({GlobalPosition.X:0.00}, {GlobalPosition.Y:0.00}, {GlobalPosition.Z:0.00})");
             ImGui.End();
         }
     }
