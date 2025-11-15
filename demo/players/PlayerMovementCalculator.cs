448e90471887dc7e3722a0fca9264499708b585c
Graziano Bolla <grazianobolla@gmail.com>
2025-11-15 17:41:56 -0300

    removed unused method

--- demo/players/PlayerMovementCalculator.cs
+++ demo/players/PlayerMovementCalculator.cs
@@ -69,16 +69,4 @@ public static class PlayerMovementCalculator
     {
         return (input & (byte)flag) > 0;
     }
-
-    public static byte GetCurrentPressedKeys()
-    {
-        byte keys = 0;
-        if (Input.IsActionPressed("right")) keys |= (byte)InputFlags.Right;
-        if (Input.IsActionPressed("left")) keys |= (byte)InputFlags.Left;
-        if (Input.IsActionPressed("forward")) keys |= (byte)InputFlags.Forward;
-        if (Input.IsActionPressed("backward")) keys |= (byte)InputFlags.Backward;
-        if (Input.IsActionPressed("space")) keys |= (byte)InputFlags.Space;
-        if (Input.IsActionPressed("shift")) keys |= (byte)InputFlags.Shift;
-        return keys;
-    }
 }
